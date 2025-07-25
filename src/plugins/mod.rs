2e9ec76be764afe0497cf1c4e6765cb596b63d79
Miska Alexia <alexia.miska@gmail.com>
2025-07-24 17:19:17 -0700

    convert movement to use axies rather than just buttons

--- src/plugins/mod.rs
+++ src/plugins/mod.rs
@@ -1,37 +1,5 @@
-use bevy::app::{App, Plugin, Startup};
-use bevy::asset::AssetServer;
-use bevy::gltf::GltfAssetLabel;
-use bevy::prelude::{Commands, NextState, Res, ResMut};
-use bevy::scene::SceneRoot;
-use crate::plugins::asset_managment::LoadingState;
-
 pub mod player;
 pub mod asset_managment;
-mod dev;
-
-pub struct GamePlugins;
-impl Plugin for GamePlugins {
-    fn build(&self, app: &mut App) {
-        app
-            .add_plugins(player::PlayerPlugin)
-            .add_plugins(asset_managment::AssetManagerPlugin)
-            .add_systems(Startup, spawn_test_level)
-        ;
-
-        // this is only needed to send components to blender,
-        // thus it is not needed in release builds
-        #[cfg(feature = "dev")]
-        app.add_plugins(dev::DevPlugins);
-    }
-}
-
-fn spawn_test_level(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut loading_state: ResMut<LoadingState>,
-){
-    *loading_state = LoadingState::Loading;
-    commands.spawn(SceneRoot(
-        asset_server.load(GltfAssetLabel::Scene(1).from_asset("belladonna-sherbet.gltf"))
-    ));
-}
+pub mod dev;
+pub mod input;
+pub mod game;
--- src/plugins/player.rs
+++ src/plugins/player.rs
@@ -3,6 +3,7 @@ use bevy::prelude::*;
 use bevy_tnua::prelude::{TnuaBuiltinJump, TnuaBuiltinWalk, TnuaController};
 use bevy_tnua::TnuaUserControlsSystemSet;
 use bevy_tnua_avian3d::TnuaAvian3dSensorShape;
+use crate::plugins::input::InputAction;
 use crate::prelude::asset_managment::LoadingData;
 
 #[derive(Component, Reflect)]
@@ -20,7 +21,7 @@ impl Plugin for PlayerPlugin {
             .register_type::<Player>()
             .register_type::<PlayerSpawn>()
             .add_observer(spawn_player)
-            .add_systems(FixedUpdate, apply_controls.in_set(TnuaUserControlsSystemSet))
+            .add_systems(FixedUpdate, apply_movement.in_set(TnuaUserControlsSystemSet))
         ;
     }
 }
@@ -42,28 +43,25 @@ fn spawn_player(
         Player,
         RigidBody::Dynamic,
         TnuaController::default(),
-        TnuaAvian3dSensorShape(Collider::cylinder(0.33, 0.0)),
         LockedAxes::ROTATION_LOCKED.unlock_rotation_y(),
         ));
 }
-fn apply_controls(keyboard: Res<ButtonInput<KeyCode>>, mut query: Query<&mut TnuaController>) {
+fn apply_movement(
+    mut input_events: EventReader<InputAction>,
+    mut query: Query<&mut TnuaController>
+) {
     let Ok(mut controller) = query.single_mut() else {
         return;
     };
-
     let mut direction = Vec3::ZERO;
-
-    if keyboard.pressed(KeyCode::ArrowUp) {
-        direction -= Vec3::Z;
-    }
-    if keyboard.pressed(KeyCode::ArrowDown) {
-        direction += Vec3::Z;
-    }
-    if keyboard.pressed(KeyCode::ArrowLeft) {
-        direction -= Vec3::X;
-    }
-    if keyboard.pressed(KeyCode::ArrowRight) {
-        direction += Vec3::X;
+    for event in input_events.read(){
+        match event {
+            InputAction::MoveAxis(axis) => {
+                direction.x = axis.x;
+                direction.z = axis.y;
+            }
+            _ => {}
+        }
     }
 
     // Feed the basis every frame. Even if the player doesn't move - just use `desired_velocity:
@@ -75,20 +73,9 @@ fn apply_controls(keyboard: Res<ButtonInput<KeyCode>>, mut query: Query<&mut Tnu
         desired_forward: Dir3::new(direction).ok(),
         // The `float_height` must be greater (even if by little) from the distance between the
         // character's center and the lowest point of its collider.
-        float_height: 1.1,
+        float_height: 1.0,
         // `TnuaBuiltinWalk` has many other fields for customizing the movement - but they have
         // sensible defaults. Refer to the `TnuaBuiltinWalk`'s documentation to learn what they do.
         ..Default::default()
     });
-
-    // Feed the jump action every frame as long as the player holds the jump button. If the player
-    // stops holding the jump button, simply stop feeding the action.
-    if keyboard.pressed(KeyCode::Space) {
-        controller.action(TnuaBuiltinJump {
-            // The height is the only mandatory field of the jump button.
-            height: 4.0,
-            // `TnuaBuiltinJump` also has customization fields with sensible defaults.
-            ..Default::default()
-        });
-    }
 }
